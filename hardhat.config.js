e24f439930a4b19864709264f79d94f7b6219714
alpinistdev <tony715225@gmail.com>
2024-01-29 20:30:48 +0800

    project init

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    //  unused configuration commented out for now
+    //  mumbai: {
+    //    url: "https://rpc-mumbai.maticvigil.com",
+    //    accounts: [process.env.privateKey]
+    //  }
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
